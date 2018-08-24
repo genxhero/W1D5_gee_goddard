8a557abe37fda1431c6995d492ff7bac74c0d28c
Aaron Goddard <aaronbear@gmail.com>
2018-08-24 12:10:45 -0700

    Initialize PolyTreeNode, create getters and setters

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,32 @@
 class PolyTreeNode
-
+  def initialize(value)
+    @children = []
+    @value = value
+    @parent = nil
+  end
+  
+  def parent
+    @parent    
+  end
+  
+  def children
+    @children
+  end
+  
+  def value
+    @value
+  end
+  
+  def parent=(new_parent)
+    current_parent = @parent
+    if current_parent
+      current_parent.children.delete(self)
+    end
+    @parent = new_parent
+    new_parent.children << self unless new_parent.nil? || new_parent.children.include?(self)
+  end
+  
+  def inspect
+    "OBJECTID: #{object_id} VALUE: #{value} CHILDREN: #{children}"
+  end
 end
