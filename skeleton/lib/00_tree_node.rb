41035d8d7e0b126732c1163b3282ed2767cb9177
Aaron Goddard <aaronbear@gmail.com>
2018-08-24 14:41:18 -0700

    Implement DFS

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class PolyTreeNode
   def initialize(value)
     @children = []
@@ -24,6 +25,27 @@ class PolyTreeNode
     end
     @parent = new_parent
     new_parent.children << self unless new_parent.nil? || new_parent.children.include?(self)
+  end
+  
+  def add_child(child_node)
+    self.children << child_node
+    child_node.parent = self
+  end 
+  
+  def remove_child(child_node)
+    raise "<<NOT A CHILD>>" if self.children.include?(child_node) == false
+    self.children.delete(child_node)
+    child_node.parent = nil
+  end
+  
+  def dfs(target_value)
+    debugger
+    return self if @value == target_value
+    @children.each do |child|
+      result = child.dfs(target_value)
+      return result unless result.nil?
+    end
+    nil
   end
   
   def inspect
